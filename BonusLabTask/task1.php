dd2a9bf16d07638d230884bdbfbadad3fcabbce6
Toukir Siddique <toukir.madrid@gmail.com>
2021-02-12 21:58:01 +0600

    Bonus Task1

--- BonusLabTask/task1.php
+++ BonusLabTask/task1.php
@@ -2,6 +2,6 @@
     $color=array('white','green','red','blue','black');
     
     echo "The memory of that scene for me is like a frame of film forever frozen at that moment:
-        the $color[2] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new
+        the ".$color[2]." carpet, the ".$color[1]." lawn, the ".$color[0]." house, the leaden sky. The new
         president and his first lady. - Richard M. Nixon";
 ?>
