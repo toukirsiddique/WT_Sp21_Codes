df74bc0063faffacc194ead62006a03a13f404a7
Toukir Siddique <toukir.madrid@gmail.com>
2021-02-12 22:43:44 +0600

    Bonus Task

---
+++ BonusLabTask/task2.php
@@ -0,0 +1,16 @@
+<html>
+    <head></head>
+    <body>
+    <?php
+        $color=array('white','green','red');
+
+        echo "$color[0], $color[1], $color[2]";
+    ?>
+
+    <ul>
+        <li>green</li>
+        <li>red</li>
+        <li>white</li>
+    </ul>
+    </body>
+</html>
---
+++ BonusLabTask/task5.php
@@ -0,0 +1,8 @@
+<?php
+    $color=array(4=>'white', 6=>'green',11=>'red');
+    foreach($color as $color)
+        {
+            echo $color;
+            break;
+        }
+?>
